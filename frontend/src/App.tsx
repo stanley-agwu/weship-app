a3bcc572b6eb45c42e842f7b059fc57742c89c16
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-10-07 19:04:53 +0200

    weShip MERN - frontend - installed react router dom and created pages.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const App = () => {
   return (
-    <div className="App">
-      My App
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
