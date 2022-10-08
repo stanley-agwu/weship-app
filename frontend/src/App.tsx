0cd9450aa77c2c32c1fec82ba9438e9704d00442
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-10-08 09:36:02 +0200

    weShip MERN - frontend - created Header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
