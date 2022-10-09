6add889a2674dd2b72b5fa6883601b519dbeb63d
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2022-10-09 17:24:38 +0200

    weShip MERN - frontend - protect the dashboard route, for only logged in user

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Dashboard = () => {
+import { useAppSelector } from '../app/hooks';
+import { getAuthState } from '../features/auth/getters';
+
+const Dashboard: React.FC = () => {
+  const navigate = useNavigate();
+  const { user } = useAppSelector(getAuthState);
+
+  useEffect(() => {
+    if (!user) {
+      navigate('/login');
+    }
+  }, [navigate, user])
   return (
     <div>Dashboard</div>
   )
 }
 
-export default Dashboard
+export default Dashboard;
