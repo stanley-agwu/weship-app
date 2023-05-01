779f70a4e3f6f7051c2a9cd3825704923e970beb
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-05-02 01:38:03 +0200

    weShip MERN - frontend - updated more component with popover

--- frontend/src/components/Table/More/More.tsx
+++ frontend/src/components/Table/More/More.tsx
@@ -1,13 +1,35 @@
-import { FunctionComponent } from 'react';
+import { Fragment, FunctionComponent, useRef, useState } from 'react';
 import { ReactComponent as MoreIcon } from '../../../assets/more.svg';
 import styles from './More.module.scss';
+import Popover from '@mui/material/Popover';
 
 const More: FunctionComponent = (): JSX.Element => {
+  const anchorRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <button className={styles.cellBtn} onClick={() => {}}>
-      <span>More</span>
-      <MoreIcon className={styles.more} />
-    </button>
+    <Fragment>
+      <Popover
+        open={isOpen}
+        anchorEl={anchorRef.current}
+        onClose={() => setIsOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 10,
+          horizontal: 0,
+        }}
+        classes={{ paper: styles.popoverClass }}
+      >
+        Popover Content
+      </Popover>
+      <button ref={anchorRef} className={styles.cellBtn} onClick={() => setIsOpen(true)}>
+        <span>More</span>
+        <MoreIcon className={styles.more} />
+      </button>
+    </Fragment>
   );
 };
 
