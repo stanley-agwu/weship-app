ec95412e1f459e76ab7a5a3ac2e13d183918b655
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-05-05 06:10:12 +0200

    weShip MERN(frontend, backend): updated eslint-config-utility package to lastest version and created .eslint and .eslintignore files for frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['utility', 'utility/import'],
+  parserOptions: {
+    project: './frontend/tsconfig.json'
+  },
+  rules: {
+    'no-underscore-dangle': 1,
+    'no-console': 2,
+    'react/react-in-jsx-scope': 0,
+  }
+};
